4c8fc7c45bc574bc88d859cbd22946f680c74729
jamie stephens <jamiestephens@jamies-Mac-mini.local>
2026-01-09 09:26:17 +0000

    Fix PWA install scope

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,11 @@
-self.addEventListener("install", () => {
+self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
-  self.clients.claim();
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", () => {
+  // Required so Chrome treats this as a full PWA
 });
